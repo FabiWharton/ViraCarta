bcf6513a6c5f0fe88c87ef14d02492b5fa27b978
FabiWharton <166619630+FabiWharton@users.noreply.github.com>
2024-10-29 10:06:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criarCartao(
+    'Programação',
+   ' O que é JavaScript?',
+    'O JavaScript é unma linguagem de programação'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criarCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos que executa alguma função'
+)
+
+criarCartao(
+    'Sociologia',
+    'O que é sociedade?',
+    'É um grupo de pessoas compartilham...'
+)
